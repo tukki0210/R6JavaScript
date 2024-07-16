14248b6b6b1bd8d6fa54b2cce75592f8dde95f29
tukki0210 <tukki0210@gmail.com>
2024-07-16 14:28:12 +0900

    0716

--- chapter6/13-demo/js/script.js
+++ chapter6/13-demo/js/script.js
@@ -1,16 +1,28 @@
-// 監視対象が範囲内に現れたら実行する動作
+// １.監視ロボットに、してほしい内容
+// 監視対象が範囲内に現れたら実行する内容
 const showKirin = (entries) => {
+  // 設定データの配列
+  console.log(entries)
+  // 配列の０番に入っているオブジェクト
+  console.log(entries[0])
+  // オブジェクトが持つtarget要素
+  console.log(entries[0].target)
+
   const keyframes = {
-    opacity: [0, 1],
+    opacity: [0,1],
     translate: ['200px 0', 0],
+    rotate: ['3600deg',0]
   };
-  //console.log('キリンさんです');
-  //console.log(entries[0].target);
-  entries[0].target.animate(keyframes, 600);
-};
   
-// 監視ロボットの設定
+  entries[0].target.animate(keyframes, 600)
+}
+
+// ２.新しい名前をつけて監視ロボットを導入する
+// 監視ロボットを設定
+
+// ３.監視ロボットに１で決めた動作内容を指示する
+// 空の監視ロボットに実行内容showKirinを与えた
 const kirinObserver = new IntersectionObserver(showKirin);
 
-// #kirinを監視するよう指示
-kirinObserver.observe(document.querySelector('#kirin'));
+// ４.監視ロボットに監視対象を教えて実行する
+kirinObserver.observe(document.querySelector('#kirin'))
